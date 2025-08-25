dc750b9faae4e1e0281f88879deb6d03bd62b87c
Ivan <imillan@revaicare.com>
2025-08-25 20:08:48 -0300

    Renombrar el middleware de caché a noCacheMiddleware y actualizar la configuración de encabezados para deshabilitar el almacenamiento en caché. Se eliminaron las funciones relacionadas con la obtención de miniaturas y se implementó la carga de miniaturas con validación de tipo de contenido y almacenamiento en el sistema de archivos. Se añadieron nuevas dependencias de AWS en go.mod y se actualizaron los archivos go.sum.

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +35,60 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
+	const maxMemory = 10 << 20 // 10 MB
+	r.ParseMultipartForm(maxMemory)
 
-	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
 
-	// TODO: implement the upload here
+	mediaTypeStruct, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Content-Type for thumbnail", err)
+		return
+	}
+	if mediaTypeStruct != "image/jpeg" && mediaTypeStruct != "image/png" {
+		respondWithError(w, http.StatusBadRequest, "Invalid Content-Type for thumbnail", nil)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	fileExtension := strings.Split(mediaTypeStruct, "/")[1]
+
+	randomBytes := make([]byte, 32)
+	rand.Read(randomBytes)
+	basePath := "assets"
+	fileName := base64.RawURLEncoding.EncodeToString(randomBytes) + "." + fileExtension
+	filePath := filepath.Join(basePath, fileName)
+
+	newFile, err := os.Create(filePath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error creating file", err)
+		return
+	}
+	defer newFile.Close()
+	io.Copy(newFile, file)
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't find video", err)
+		return
+	}
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Not authorized to update this video", nil)
+		return
+	}
+
+	url := fmt.Sprintf("http://localhost:%s/assets/%s", cfg.port, fileName)
+	video.ThumbnailURL = &url
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
